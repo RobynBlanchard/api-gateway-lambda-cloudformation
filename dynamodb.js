f2aafeb076005a451f22f71676db069ee68e2b59
Robyn Blanchard <robynblanchard@notonthehighstreet.com>
2018-11-21 14:20:04 +0000

    Store products in DynamoDB (WIP)

---
+++ dynamodb.js
@@ -0,0 +1,98 @@
+var AWS = require('aws-sdk');
+var credentials = new AWS.SharedIniFileCredentials({profile: 'robyn'});
+AWS.config.credentials = credentials;
+AWS.config.update({region: 'eu-west-1'});
+
+// Create the DynamoDB service object
+ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+// var params = {
+//   TableName: 'Products',
+// //   Item: {
+// //     'ProductID' : {N: '1'},
+// //     'Title' : {S: 'little bag of happiness'},
+// //     'Price' : {N: '2500'}
+// //   }
+//     Item: {
+//         'ProductID' : {N: '5'},
+//         'Title' : {S: 'lava lamp'},
+//         'Price' : {N: '0850'}
+//     }
+// };
+
+// // Call DynamoDB to add the item to the table
+// ddb.putItem(params, function(err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } else {
+//     console.log("Success", data);
+//   }
+// });
+
+var params2 = {
+  RequestItems: {
+    "Products": [
+      {
+        PutRequest: {
+          Item: {
+            'ProductID': { N: '6' },
+            'Title': { S: 'pewter photo frame' },
+            'Price': { N: '3200' }
+          }
+        }
+      },
+      {
+        PutRequest: {
+          Item: {
+            'ProductID': { N: '7' },
+            'Title': { S: 'birthstone necklace' },
+            'Price': { N: '4500' }
+          }
+        }
+      },
+      {
+        PutRequest: {
+          Item: {
+            'ProductID': { N: '8' },
+            'Title': { S: 'life sized cow' },
+            'Price': { N: '78000' }
+          }
+        }
+      }
+    ]
+  }
+};
+// ddb.batchWriteItem(params2, function(err, data) {
+//     if (err) {
+//       console.log("Error", err);
+//     } else {
+//       console.log("Success", data);
+//     }
+// });
+
+var params = {
+  Key: {
+   "ProductID": {
+     N: "1"
+    },
+  },
+  TableName: "Products"
+ };
+//  ddb.getItem(params, function(err, data) {
+//    if (err) console.log(err, err.stack); // an error occurred
+//    else     console.log(data);           // successful response
+//  });
+
+
+var params3 = {
+  TableName: "Products"
+ };
+ ddb.scan(params3, function(err, data) {
+  if (err) {
+    console.log(err, err.stack); // an error occurred
+  } else {
+    data.Items.forEach(e => {
+      console.log(e.ProductID, e.Title, e.Price);
+    });
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-const responses = require('./responses');
+// const responses = require('./responses');
+// var AWS = require('aws-sdk');
+// var credentials = new AWS.SharedIniFileCredentials({profile: 'robyn'});
+// AWS.config.credentials = credentials;
+// AWS.config.update({region: 'eu-west-1'});
 
-const db = [
-  {
-    id: 1,
-    title: "little bag of happiness",
-    price: 2500
-  },
-  {
-    id: 2,
-    title: "personalised scarf",
-    price: 1250
-  }
-];
+// // Create the DynamoDB service object
+// ddb = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+
+
+getProducts = () => {
+  var params = {
+    TableName: "Products"
+   };
+   ddb.scan(params, function(err, data) {
+    if (err) {
+      console.log(err, err.stack); // an error occurred
+    } else {
+      let items_;
+      data.Items.forEach(e => {
+        console.log(e.ProductID, e.Title, e.Price);
+        items_.push([e.ProductID, e.Title, e.Price]);
+      });
+      return items_
+    }
+  });
+}
+
+getProduct = id => {
+  var params = {
+    Key: {
+     "ProductID": {
+       N: `"${id}"`
+      },
+    },
+    TableName: "Products"
+   };
+  ddb.getItem(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     return data           // successful response
+  });
+}
 
 exports.router = (event, context, callback) => {
   const path = event.path;
@@ -20,11 +49,12 @@ exports.router = (event, context, callback) => {
 
   if (path === "/products" && httpMethod === "GET") {
     if (queryParams === null) {
+      const products = getProducts();
       var response = responses.success(db);
     } else if (queryParams.hasOwnProperty('id')) {
       const productID = queryParams.id;
-      const productIndex = db.findIndex(el => el.id.toString() === productID);
-      var response = responses.success(db[productIndex]);
+      const product = getProduct(productID)
+      var response = responses.success(product);
     } else {
       var response =  responses.notFound();
     }
